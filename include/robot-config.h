1335a932b78a56eae6bfcef9d9cd04d87713fdc6
EthanGuill <ethan8402@outlook.com>
2024-02-24 02:45:46 -0600

    Skills version

--- include/robot-config.h
+++ include/robot-config.h
@@ -4,7 +4,7 @@ extern brain Brain;
 
 // VEXcode devices
 extern controller Controller1;
-extern smartdrive Drivetrain;
+extern drivetrain Drivetrain;
 extern motor leftIntake;
 extern motor rightIntake;
 extern motor leftCatapault;
